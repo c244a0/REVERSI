7a8f76bb68cb043c89ea3a2c43c22db403db5469
c244a0 <108412889+c244a0@users.noreply.github.com>
2023-11-05 01:41:47 +0900

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "cypress";
 
 module.exports = defineConfig({
   e2e: {
-    supportFile: "cypress/support/e2e.ts",
-    specPattern: "**/cypress/e2e/*.cy.js"
+    supportFile: "cypress/support/e2e.ts"
   },
 });
