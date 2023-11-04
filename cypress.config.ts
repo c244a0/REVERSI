3d6927d0628ff8bf63e1b5e7775feebd33dde1ba
c244a0 <108412889+c244a0@users.noreply.github.com>
2023-11-05 01:57:14 +0900

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "cypress";
 module.exports = defineConfig({
   e2e: {
     supportFile: "cypress/support/e2e.ts",
-    basUrl: "http://52.195.45.7:3000"
+    baseUrl: "http://52.195.45.7:3000"
   },
 });
