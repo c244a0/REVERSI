de49c0bec1627a9e0de80fe3f68fc12790daf7fa
c244a0 <108412889+c244a0@users.noreply.github.com>
2023-11-07 23:50:32 +0900

    delete URL

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "cypress";
 
 module.exports = defineConfig({
   e2e: {
-    supportFile: "cypress/support/e2e.ts",
-    baseUrl: "http://52.195.45.7:3000"
+    supportFile: "cypress/support/e2e.ts"
   },
 });
