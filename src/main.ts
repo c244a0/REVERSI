37e9bdd6b57af72a2778256b06bc51938cc0ed55
c244a0 <108412889+c244a0@users.noreply.github.com>
2023-12-07 02:41:58 +0900

    Revert "add basic certification"

--- src/main.ts
+++ src/main.ts
@@ -5,39 +5,10 @@ import { DomainError } from "./domain/error/DomainError";
 import { gameRouter } from "./presentation/gameRouter";
 import { turnRouter } from "./presentation/turnRouter";
 import { ApplicationError } from "./application/error/applicationError";
-import basicAuth from 'express-basic-auth';
-require('dotenv').config();
-
 
 const PORT = 3000;
 
 const app = express();
-
-function getEnvVariable(key: string): string {
-  const value = process.env[key];
-  if (value === undefined) {
-    throw new Error(`環境変数 ${key} が設定されていません。`);
-  }
-  return value;
-}
-
-// 環境変数を取得
-const basicId = getEnvVariable('BASIC_ID');
-const basicPassword = getEnvVariable('BASIC_PASSWORD');
-
-
-
-// Basic認証の設定
-const users = { 
-  [basicId]: basicPassword 
-};
-
-app.use(basicAuth({
-    users: users,
-    challenge: true, 
-    realm: 'REVERSI'
-}));
-
 
 app.use(morgan("dev"));
 app.use(express.static("static", { extensions: ["html"] }));
@@ -48,7 +19,9 @@ app.use(turnRouter);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => {
+  console.log(`Reversi application started: http://localhost:${PORT}`);
+});
 
 interface ErrorResponseBody {
   type: string;
